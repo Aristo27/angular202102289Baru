e3c99e70d4f192ae38b9081b40f6cff3f7a65e75
Aristo <106814779+Aristo27@users.noreply.github.com>
2022-12-16 22:36:50 +0700

    forex

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { ForexComponent } from './forex/forex.component';
 import { Dashboard3Component } from './dashboard3/dashboard3.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path:"admin", component: AdminComponent},
   { path: "dashboard", component: DashboardComponent, canActivate : [OtentikasiGuard]},
   { path: "dashboard2", component: Dashboard2Component, canActivate : [OtentikasiGuard]},
+  { path: "forex", component: ForexComponent, canActivate : [OtentikasiGuard]},
   { path: "dashboard3", component: Dashboard3Component},
   { path: "login", component: LoginComponent},
   { path: "register", component: RegisterComponent},
